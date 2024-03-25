c73fced6c5eb100a644c2662cfc097544530ac35
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2024-03-25 14:18:21 +0200

    Install react-router-dom, do basic setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css'; 
 import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:
+      <App />,
+    //errorElement
+  },
+  /*
+  {
+    path: "about",
+    element:
+      <HomePage
+        ApiUrl="https://dgdemo23.tundramedia.net/api/api.php?albumid=2059494&verify=true"
+        ContentAbout
+        MetaTitle="About | Directors Guild"
+      />,
+  },
+  */
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
