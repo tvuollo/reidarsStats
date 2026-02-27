24275e3118b5e3cf6795a2257a5edbd8fd7ea11e
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2026-02-27 09:50:21 +0200

    Add loading state and loader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 
-import { useMemo } from 'react'
+import { type ReactElement, useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
+import LoadingView from './components/LoadingView'
 import { getSingleGameViewData, hasSingleGameEventsData } from './features/gameEvents'
 import { searchGames } from './features/gameSearch'
 import {
@@ -15,40 +16,42 @@ import SearchPage from './views/SearchPage'
 import SeasonDetailPage from './views/SeasonDetailPage'
 import SingleGamePage from './views/SingleGamePage'
 
-function App() {
-  const [searchParams] = useSearchParams()
+interface ViewResolutionInput {
+  view: string
+  seasonKey: string
+  statGroupId: string
+  gameId: string
+  query: string
+}
 
-  const view = (searchParams.get('view') ?? 'home').toLowerCase()
-  const seasonKey = searchParams.get('file') ?? ''
-  const statGroupId = searchParams.get('statgroupid') ?? ''
-  const gameId = searchParams.get('gameid') ?? ''
-  const query = searchParams.get('query')?.trim() ?? ''
+function resolveViewContent({
+  view,
+  seasonKey,
+  statGroupId,
+  gameId,
+  query,
+}: ViewResolutionInput): ReactElement {
+  if (view === 'season' && seasonKey && statGroupId) {
+    const detail = getReidarsStatGroupDetail(seasonKey, statGroupId)
+    return (
+      <SeasonDetailPage
+        detail={detail}
+        seasonKey={seasonKey}
+        statGroupId={statGroupId}
+        hasGameEventsData={hasSingleGameEventsData}
+      />
+    )
+  }
 
-  const content = useMemo(() => {
-    if (view === 'season' && seasonKey && statGroupId) {
-      const detail = getReidarsStatGroupDetail(seasonKey, statGroupId)
-      return (
-        <SeasonDetailPage
-          detail={detail}
-          seasonKey={seasonKey}
-          statGroupId={statGroupId}
-          hasGameEventsData={hasSingleGameEventsData}
-        />
-      )
-    }
+  if (view === 'game' && gameId) {
+    const data = getSingleGameViewData(gameId)
+    return <SingleGamePage data={data} gameId={gameId} />
+  }
 
-    if (view === 'game' && gameId) {
-      const data = getSingleGameViewData(gameId)
-      return <SingleGamePage data={data} gameId={gameId} />
-    }
-
-    if (view === 'search' && query) {
-      const results = searchGames(query)
-      return <SearchPage query={query} results={results} />
-    }
-
-    return <DashboardPage totals={reidarsTotals} />
-  }, [view, seasonKey, statGroupId, gameId, query])
+  if (view === 'search' && query) {
+    const results = searchGames(query)
+    return <SearchPage query={query} results={results} />
+  }
 
   if (view !== 'home' && !seasonKey && !statGroupId && !gameId && !query) {
     return (
@@ -63,7 +66,40 @@ function App() {
     )
   }
 
-  return content
+  return <DashboardPage totals={reidarsTotals} />
+}
+
+function App() {
+  const [searchParams] = useSearchParams()
+
+  const view = (searchParams.get('view') ?? 'home').toLowerCase()
+  const seasonKey = searchParams.get('file') ?? ''
+  const statGroupId = searchParams.get('statgroupid') ?? ''
+  const gameId = searchParams.get('gameid') ?? ''
+  const query = searchParams.get('query')?.trim() ?? ''
+  const routeKey = `${view}|${seasonKey}|${statGroupId}|${gameId}|${query}`
+
+  const [resolved, setResolved] = useState<{ routeKey: string, content: ReactElement }>(() => ({
+    routeKey,
+    content: resolveViewContent({ view, seasonKey, statGroupId, gameId, query }),
+  }))
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      setResolved({
+        routeKey,
+        content: resolveViewContent({ view, seasonKey, statGroupId, gameId, query }),
+      })
+    }, 0)
+
+    return () => window.clearTimeout(timeoutId)
+  }, [routeKey, view, seasonKey, statGroupId, gameId, query])
+
+  if (resolved.routeKey !== routeKey) {
+    return <LoadingView />
+  }
+
+  return resolved.content
 }
 
 export default App
---
+++ src/components/LoadingView.tsx
@@ -0,0 +1,11 @@
+function LoadingView() {
+  return (
+    <main className="container reidars-stats-container">
+      <div style={{ margin: "80px auto", textAlign: "center" }}>
+        <img className="reidars-loader" src="https://reidarshockey.com/wp-content/themes/reidarstheme24/assets/img/logo480.png" alt="" style={{ height: "auto", width: 240 }} />
+      </div>
+    </main>
+  )
+}
+
+export default LoadingView
